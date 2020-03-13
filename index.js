81dc1efc7b9c373704cb5176e2604bb51eb947a7
kartikeychandraa <kartikeychandraa@gmail.com>
2020-03-13 11:24:37 +0530

    new

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 var express = require("express");
 var body = require("body-parser");
 var app = express();
+//mongoose import
+var mongoose = require("mongoose");
+
+const UserModel = require("./model/user");
+
+mongoose.connect(
+  "mongodb+srv://kartikey:chandra%40123@cluster0-xwnrl.mongodb.net/test?retryWrites=true&w=majority",
+  { useUnifiedTopology: true, useNewUrlParser: true }
+);
+//----------------
 app.set("view engine", "ejs");
 app.use(express.urlencoded());
 app.use(express.json());
+app.use(body.urlencoded({ extended: false }));
+app.use(body.json());
 var ejs = require("ejs");
 app.get("/", function(req, res) {
   res.render("index");
@@ -21,6 +33,47 @@ app.post("/submit", (req, res) => {
   console.log(a);
   console.log(b);
 });
+//mongoose 
+app.get("/input", async (req, res) => {
+  const userr = new UserModel({
+    name: "a+b",
+    email: "c",
+    password: "d",
+  });
+  try {
+    await userr.save();
+    res.send(userr);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.get("/output", async (req, res) => {
+  const data = await UserModel.find({});
+  try {
+    res.send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.get("/delete", async (req, res) => {
+  const data = await UserModel.findOneAndRemove({name: "kartikey",
+    email: "xom",
+    password: "lo"});
+  try {
+    res.send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+//mongoose basic ends
+
+
+
+
+
+
 
 app.listen(process.env.PORT || 3000);
 console.log("SERVER RUNNING");
---
+++ model/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const userrrr = mongoose.model("userrrr", UserSchema);
+module.exports = userrrr;
